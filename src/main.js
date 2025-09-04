28d782455b929b3d6ce50785a5180f7b0a3de7ab
Phạm Văn Mộng <isotemplates@gmail.com>
2025-09-04 08:06:08 +0700

    Fix unhead
    Fix unhead error

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createHead } from '@unhead/vue'
+
+import { createUnhead } from 'unhead'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-const head = createHead()
+const unhead = createUnhead()
 
 app.use(router)
 
-app.use(head)
+app.use(unhead)
 
 app.mount('#app')
 
